a700e9c70ce7e64c590ba38fc329a8ffc506cd36
Priya Dharshini G <131358891+Priyadharshini1301@users.noreply.github.com>
2023-08-08 23:17:00 +0530

    First commit done

---
+++ src/index.js
@@ -0,0 +1,19 @@
+console.log("The server is running");
+console.log("This is Priya!");
+
+var num = 5;
+var length = 4;
+
+function calculateSquare(number)
+{
+    return number*number;
+}
+var square = calculateSquare(num);
+console.log(`The square of ${num} is ${square}`);
+
+function FindArea(length)
+{
+    return length*length;
+}
+var area = FindArea(length);
+console.log(`The area of the square with length ${length} is ${area}`);
